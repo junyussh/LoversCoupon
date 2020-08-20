2e92f1b6969a1a087be2fbcab1aa9a9c247a0f3a
oxygen-TW <howard2805@gmail.com>
2020-08-20 23:43:45 +0800

    Replace HTML to Angular component

--- frontend/LoversCoupon/src/app/app.module.ts
+++ frontend/LoversCoupon/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule
