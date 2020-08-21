3f9345df6575696a186cb6c48bf6e4c2df7b9b8d
oxygen-TW <howard2805@gmail.com>
2020-08-21 23:29:31 +0800

    Add basic router code

--- frontend/LoversCoupon/src/app/app.module.ts
+++ frontend/LoversCoupon/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule ,Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { MainComponent } from "./main/main.component";
+import { TicketsComponent } from './tickets/tickets.component';
+ 
+const routes: Routes = [
+  {path: "", component: MainComponent},
+  {path: "add", component: AddComponent},
+  {path: "tickets", component: TicketsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    TicketsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule,  RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
