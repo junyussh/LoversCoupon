a491262afc34abfeaf35ecc934f1dc1e0480a3bb
oxygen-TW <howard2805@gmail.com>
2020-08-21 23:56:45 +0800

    Add handler service

--- frontend/LoversCoupon/src/app/main/main.component.ts
+++ frontend/LoversCoupon/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HandlerService } from "../handler.service";
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: HandlerService) {}
+
+  setUser(_user){
+    //this.service.user = _user;
+    console.log(this.service.user);
+  }
 
   ngOnInit(): void {
   }
